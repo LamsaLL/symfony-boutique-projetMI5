44b07b3c49517a7712f20f20c43a3a53c73bf8f9
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-01-07 12:46:34 +0100

    add some pages

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+// Controller/DefaultController.php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class DefaultController extends AbstractController {
+    public function index() {
+        return $this->render('home.html.twig');
+    }
+
+//     public function hello($userName) {
+//         return $this->render( 'hello.html.twig', ['userName' => $userName ]);
+//     }
+}
