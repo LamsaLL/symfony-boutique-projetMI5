23ad9c0451d1ad6a551007899999800c2cc5b142
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-01-19 11:45:36 +0100

    refactor(ContactController): renaming

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class contactController extends AbstractController {
-    public function contact() {
+class ContactController extends AbstractController {
+    public function index() {
         return $this->render('contact.html.twig');
     }
 
