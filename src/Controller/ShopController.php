739eba4010057e7edfdc8a4e29f2b52fdc31c894
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-01-19 11:44:00 +0100

    feat(ShopController): add department action

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -3,12 +3,21 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\ShopService;
-
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 class ShopController extends AbstractController {
-    public function index(ShopService $shop) {
-        $categories = $shop->findAllCategories();
+    public function index(CategoryRepository $categoryRepository) {
+        $categories = $categoryRepository->findAll();
         return $this->render('shop.html.twig', [
             "categories" => $categories
+        ]);
+    }
+
+    public function department($categoryId, ProductRepository $productRepository) {
+        // Show all products with the given categoryId
+        $products = $productRepository->findBy(["category" => $categoryId]);
+        return $this->render('shop.html.twig', [
+           "products" => $products
         ]);
     }
 }
