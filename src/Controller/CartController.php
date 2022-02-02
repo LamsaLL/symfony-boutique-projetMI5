bc70766faa0bede7d5979072dfa827d811ae4388
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-02 11:43:31 +0100

    feat(Cart): add cart_add and cart_remove routes

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,13 +3,30 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\ProductRepository;
+use App\Service\CartService;
 
 class CartController extends AbstractController {
-    public function index(ProductRepository $productRepository) {
-        $products = $productRepository->findAll();
+    public function index(CartService $cart) {
+        $products = $cart->getContent();
         return $this->render('cart/index.html.twig', [
-            "categories" => $products
+            'products' => $products,
         ]);
+    }
+
+    //TODO: set $quantity to 1 if it is not set
+    public function add($productId, $quantity, CartService $cart) {
+        // add new product to cart
+        $cart->addProduct($productId, $quantity);
+        // redirect to cart page and index route
+        return $this->redirectToRoute('cart_index');
+    }
+
+    //TODO: set $quantity to 1 if it is not set
+    public function remove($productId, $quantity, CartService $cart) {
+        // remove product to cart
+        $cart->removeProduct($productId, $quantity);
+        // redirect to cart page and index route
+        return $this->redirectToRoute('cart_index');
     }
 
     public function validation(){
