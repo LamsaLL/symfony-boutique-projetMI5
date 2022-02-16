63a63670db13de685149a79392ecde5257c47eb3
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-16 18:33:22 +0100

    feat: add search route to find products by name

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -1,27 +1,51 @@
 <?php
 // Controller/shopController.php
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\ShopService;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 
-class ShopController extends AbstractController {
-    public function index(CategoryRepository $categoryRepository) {
+
+class ShopController extends AbstractController
+{
+    public function index(CategoryRepository $categoryRepository)
+    {
         $categories = $categoryRepository->findAll();
         return $this->render('shop.html.twig', [
             "categories" => $categories
         ]);
     }
 
-    public function department($categoryId, ProductRepository $productRepository, CategoryRepository $categoryRepository) {
+    // Search products by name
+    public function search($search, ProductRepository $productRepository)
+    {
+        $products = $productRepository->findByName($search);
+
+        // Count the number of products found
+        $count = count($products);
+
+        return $this->render('department.html.twig', [
+            'products' => $products,
+            'search' => $search,
+            'nbProducts' => $count
+        ]);
+    }
+
+    public function department($categoryId, ProductRepository $productRepository, CategoryRepository $categoryRepository)
+    {
         // Get the category object with given id
         $category = $categoryRepository->find($categoryId);
         // Show all products with the given categoryId
         $products = $productRepository->findBy(["category" => $categoryId]);
+        // Count the number of products found
+        $count = count($products);
         return $this->render('department.html.twig', [
-           "category" => $category,
-           "products" => $products
+            "category" => $category,
+            "products" => $products,
+            "nbProducts" => $count
         ]);
     }
 }
