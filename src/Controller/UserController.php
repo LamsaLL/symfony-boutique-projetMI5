b318a5f10745ce3cde370334db4d2e1da74ce03f
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-19 18:54:39 +0100

    style: format documents

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,34 +18,42 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 
 class UserController extends AbstractController
 {
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig');
-    }
+  public function index(UserRepository $userRepository): Response
+  {
+    return $this->render('user/index.html.twig');
+  }
 
-    public function new(Request $request, EntityManagerInterface $entityManager,
-                        UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Encoder le mot de passe qui est en clair pour l’instant
-            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
-            $user->setPassword($hashedPassword);
-            // Définir le rôle de l’usager qui va être créé
-            $user->setRoles(["ROLE_CLIENT"]);
-            // Faire persister l’usager en BD
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // Après l’inscription, rediriger vers l’authentification
-            return $this->redirectToRoute('app_login');
-        }
-        return $this->renderForm('user/new.html.twig', ['user' => $user, 'form' => $form]);
+  public function new(
+    Request $request,
+    EntityManagerInterface $entityManager,
+    UserPasswordHasherInterface $passwordHasher
+  ): Response {
+    $user = new User();
+    $form = $this->createForm(UserType::class, $user);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      // Encoder le mot de passe qui est en clair pour l’instant
+      $hashedPassword = $passwordHasher->hashPassword(
+        $user,
+        $user->getPassword()
+      );
+      $user->setPassword($hashedPassword);
+      // Définir le rôle de l’usager qui va être créé
+      $user->setRoles(["ROLE_CLIENT"]);
+      // Faire persister l’usager en BD
+      $entityManager->persist($user);
+      $entityManager->flush();
+      // Après l’inscription, rediriger vers l’authentification
+      return $this->redirectToRoute('app_login');
     }
+    return $this->renderForm('user/new.html.twig', [
+      'user' => $user,
+      'form' => $form,
+    ]);
+  }
 
-    public function orders(): Response
-    {
-        return $this->render('user/orders.html.twig');
-    }
+  public function orders(): Response
+  {
+    return $this->render('user/orders.html.twig');
+  }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -15,63 +15,84 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ProductRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Product::class);
-    }
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, Product::class);
+  }
 
-    public function createPoduct() {
-        // TODO
-        $product = new Product();
-        $product->setName('Un nouveau produit');
-        $product->setPrice(19.99);
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($product);
-        $em->flush();
-        return new Response('Produit créé avec id '.$product->getId());
-    }
+  public function createPoduct()
+  {
+    // TODO
+    $product = new Product();
+    $product->setName('Un nouveau produit');
+    $product->setPrice(19.99);
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($product);
+    $em->flush();
+    return new Response('Produit créé avec id ' . $product->getId());
+  }
 
-    public function updateProduct($id)
-    {
-        $em = $this->ZgetDoctrine()->getManager();
-        $product = $em->getRepository(Product::class)->find($id);
-        if (!$product) {
-            // throw $this->createNotFoundException('No product found for id '.$id);
-        }
-        $product->setName('New product name!');
-        $em->flush();
+  public function updateProduct($id)
+  {
+    $em = $this->ZgetDoctrine()->getManager();
+    $product = $em->getRepository(Product::class)->find($id);
+    if (!$product) {
+      // throw $this->createNotFoundException('No product found for id '.$id);
     }
+    $product->setName('New product name!');
+    $em->flush();
+  }
 
-    public function showProduct($id)
-    {
-        $product= $this->getDoctrine()->getManager()
-        ->getRepository(Product::class)
-        ->find($id);
-        // if (!$product) {
-        // throw $this->createNotFoundException('Produit non trouvé avec id '.$id);
-        // }
-    }
+  public function showProduct($id)
+  {
+    $product = $this->getDoctrine()
+      ->getManager()
+      ->getRepository(Product::class)
+      ->find($id);
+    // if (!$product) {
+    // throw $this->createNotFoundException('Produit non trouvé avec id '.$id);
+    // }
+  }
 
-    // Toutes les requêtes sont à mettre dans le repository /**
-    /**
-     * @return Product[]
-     */
-    public function findAllGreaterThanPrice($price): array {
-        $entityManager= $this->getEntityManager();
-        $query= $entityManager->createQuery(
+  // Toutes les requêtes sont à mettre dans le repository /**
+  /**
+   * @return Product[]
+   */
+  public function findAllGreaterThanPrice($price): array
+  {
+    $entityManager = $this->getEntityManager();
+    $query = $entityManager
+      ->createQuery(
         'SELECT p
         FROM App\Entity\Product p
         WHERE p.price > :price
         ORDER BY p.price ASC'
-        )->setParameter('price', $price);
-        // returns an array of Product objects
-        return $query->getResult();
-    }
+      )
+      ->setParameter('price', $price);
+    // returns an array of Product objects
+    return $query->getResult();
+  }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
+  // Find products on search string on name
+  public function findByName($search)
+  {
+    $entityManager = $this->getEntityManager();
+    $query = $entityManager
+      ->createQuery(
+        'SELECT p
+        FROM App\Entity\Product p
+        WHERE p.name LIKE :search
+        ORDER BY p.name ASC'
+      )
+      ->setParameter('search', '%' . $search . '%');
+    // returns an array of Product objects
+    return $query->getResult();
+  }
+
+  // /**
+  //  * @return Product[] Returns an array of Product objects
+  //  */
+  /*
     public function findByExampleField($value)
     {
         return $this->createQueryBuilder('p')
@@ -85,7 +106,7 @@ class ProductRepository extends ServiceEntityRepository
     }
     */
 
-    /*
+  /*
     public function findOneBySomeField($value): ?Product
     {
         return $this->createQueryBuilder('p')
