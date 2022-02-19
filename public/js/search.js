e2ddf2f13a5b90f15fac22e495a39b043ad4521b
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-19 18:54:03 +0100

    fix(search.js): location

--- public/js/search.js
+++ public/js/search.js
@@ -4,13 +4,7 @@ $(function () {
   // Comportement du bouton de recherche
   $("#searchButton").click(function () {
     var searchString = $("#searchString").val().trim();
-    // Check if the url contain en or fr
-    var lang = window.location.href.split("/")[3];
-    if (lang == "en") {
-      location = "/en/shop/search/" + searchString;
-    } else {
-      location = "/shop/search/" + searchString;
-    }
+    location = "/fr/shop/search/" + searchString;
     return false;
   });
 });
