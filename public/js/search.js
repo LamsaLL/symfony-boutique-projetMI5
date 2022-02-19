b10eb6c6b6a45e85e71bbcc62dde732daa7fac2c
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-19 17:39:33 +0100

    feat: add search all product and search like the query

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,16 @@
 $(function () {
-	// Activation tooltip bootstrap sur la page
-	$('[data-toggle="tooltip"]').tooltip()
-	// Comportement du bouton de recherche
-	$('#searchButton').click(function () {
-		var searchString = $('#searchString').val().trim()
-		location = '/shop/search/' + searchString
-		return false
-	})
-})
+  // Activation tooltip bootstrap sur la page
+  $('[data-toggle="tooltip"]').tooltip();
+  // Comportement du bouton de recherche
+  $("#searchButton").click(function () {
+    var searchString = $("#searchString").val().trim();
+    // Check if the url contain en or fr
+    var lang = window.location.href.split("/")[3];
+    if (lang == "en") {
+      location = "/en/shop/search/" + searchString;
+    } else {
+      location = "/shop/search/" + searchString;
+    }
+    return false;
+  });
+});
--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -8,47 +8,55 @@ use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 
-
 class ShopController extends AbstractController
 {
-    public function index(CategoryRepository $categoryRepository)
-    {
-        $categories = $categoryRepository->findAll();
-        // Count number of categories
-        $count = count($categories);
-        return $this->render('shop.html.twig', [
-            "categories" => $categories,
-            'nbCategories' => $count
-        ]);
+  public function index(CategoryRepository $categoryRepository)
+  {
+    $categories = $categoryRepository->findAll();
+    // Count number of categories
+    $count = count($categories);
+
+    return $this->render('shop/index.html.twig', [
+      "categories" => $categories,
+      'nbCategories' => $count,
+    ]);
+  }
+
+  // Search products by name
+  public function search($search = '', ProductRepository $productRepository)
+  {
+    // If search is empty, return all products
+    if (empty($search)) {
+      $products = $productRepository->findAll();
+    } else {
+      $products = $productRepository->findByName($search);
     }
+    // Count the number of products found
+    $count = count($products);
 
-    // Search products by name
-    public function search($search, ProductRepository $productRepository)
-    {
-        $products = $productRepository->findByName($search);
+    return $this->render('shop/department.html.twig', [
+      'products' => $products,
+      'search' => $search,
+      'nbProducts' => $count,
+    ]);
+  }
 
-        // Count the number of products found
-        $count = count($products);
+  public function department(
+    $categoryId,
+    ProductRepository $productRepository,
+    CategoryRepository $categoryRepository
+  ) {
+    // Get the category object with given id
+    $category = $categoryRepository->find($categoryId);
+    // Show all products with the given categoryId
+    $products = $productRepository->findBy(["category" => $categoryId]);
+    // Count the number of products found
+    $count = count($products);
 
-        return $this->render('department.html.twig', [
-            'products' => $products,
-            'search' => $search,
-            'nbProducts' => $count
-        ]);
-    }
-
-    public function department($categoryId, ProductRepository $productRepository, CategoryRepository $categoryRepository)
-    {
-        // Get the category object with given id
-        $category = $categoryRepository->find($categoryId);
-        // Show all products with the given categoryId
-        $products = $productRepository->findBy(["category" => $categoryId]);
-        // Count the number of products found
-        $count = count($products);
-        return $this->render('department.html.twig', [
-            "category" => $category,
-            "products" => $products,
-            "nbProducts" => $count
-        ]);
-    }
+    return $this->render('shop/department.html.twig', [
+      "category" => $category,
+      "products" => $products,
+      "nbProducts" => $count,
+    ]);
+  }
 }
